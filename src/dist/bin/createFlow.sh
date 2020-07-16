58911b8be2a4edeeb36bb3f1dc42358b1537892d
中澤杉夫 <nakazawa@nautilus-technologies.com>
2020-07-16 18:12:20 +0900

    リファクタリング

--- src/dist/bin/createFlow.sh
+++ src/dist/bin/createFlow.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-ASAKUSA_MOD=${ASAKUSA_HOME}/bin/asakusa
+ASAKUSA_CMD=${ASAKUSA_HOME}/bin/asakusa
 # batch list
-#BATCHS=`${ASAKUSA_MOD} list batch`
+#BATCHS=`${ASAKUSA_CMD} list batch`
 OUT=flowfigure
 mkdir -p ${OUT}
 
-for BATCH in `${ASAKUSA_MOD} list batch`
+for BATCH in `${ASAKUSA_CMD} list batch`
 do
   echo ${BATCH}
-  ${ASAKUSA_MOD} generate dot jobflow -o ${OUT}/${BATCH}.dot ${BATCH}
+  ${ASAKUSA_CMD} generate dot jobflow -o ${OUT}/${BATCH}.dot ${BATCH}
   dot -Tpdf ${OUT}/${BATCH}.dot > ${OUT}/${BATCH}.pdf
 
   for JOB in `~/asakusa/bin/asakusa list jobflow ${BATCH}`
   do
     echo jobflow:${JOB}
-    ${ASAKUSA_MOD} generate dot operator ${BATCH} -o ${OUT}/${JOB}.dot --jobflow ${JOB}
+    ${ASAKUSA_CMD} generate dot operator ${BATCH} -o ${OUT}/${JOB}.dot --jobflow ${JOB}
     dot -Tpdf ${OUT}/${JOB}.dot > ${OUT}/${JOB}.pdf
   done
 done
